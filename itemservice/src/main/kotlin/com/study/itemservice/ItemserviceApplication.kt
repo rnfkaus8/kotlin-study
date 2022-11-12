da068f23ec4db0a040e0a910772eebac7efb44ad
rnfkaus8 <kth4350@gmail.com>
2022-11-12 15:58:59 +0900

    itemservice study init

---
+++ itemservice/src/main/kotlin/com/study/itemservice/ItemserviceApplication.kt
@@ -0,0 +1,11 @@
+package com.study.itemservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ItemserviceApplication
+
+fun main(args: Array<String>) {
+    runApplication<ItemserviceApplication>(*args)
+}
