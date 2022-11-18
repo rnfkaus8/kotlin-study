315afb9f37af69788f8fa55e1ee9a144b9531ebd
rnfkaus8 <kth4350@gmail.com>
2022-11-18 22:09:39 +0900

    memberService 추가

---
+++ itemservice/src/main/kotlin/com/study/itemservice/service/MemberService.kt
@@ -0,0 +1,39 @@
+package com.study.itemservice.service
+
+import com.study.itemservice.domain.Member
+import com.study.itemservice.repository.MemberRepository
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+@Transactional(readOnly = true)
+class MemberService(
+    val memberRepository: MemberRepository
+) {
+
+    /**
+     * 회원가입
+     */
+    @Transactional
+    fun join(member: Member): Long {
+        validateDuplicateMember(member)
+        memberRepository.save(member)
+        return member.id
+    }
+
+    private fun validateDuplicateMember(member: Member) {
+        val findMembers = memberRepository.findByName(member.name)
+        if (findMembers.isNotEmpty()) {
+            throw IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    fun findMembers(): List<Member> {
+        return memberRepository.findAll()
+    }
+
+    fun findOne(memberId: Long): Member {
+        return memberRepository.findOne(memberId)
+    }
+
+}
