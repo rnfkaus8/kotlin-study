4c393e4a2f31d22139ad370d4e8a1b96312a6bd1
rnfkaus8 <kth4350@gmail.com>
2022-11-20 19:26:18 +0900

    비지니스 로직 추가

--- itemservice/src/main/kotlin/com/study/itemservice/domain/item/Item.kt
+++ itemservice/src/main/kotlin/com/study/itemservice/domain/item/Item.kt
@@ -1,21 +1,34 @@
 package com.study.itemservice.domain.item
 
 import com.study.itemservice.domain.Category
+import com.study.itemservice.exception.NotEnoughStockException
 import javax.persistence.*
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "dtype")
 @Table(name = "item")
-abstract class Item {
-  @Id @GeneratedValue
-  @Column(name = "item_id")
-  private var id: Long? = null
+abstract class Item(
+    var name: String,
+    var price: Int,
+    var stockQuantity: Int,
+    @ManyToMany(mappedBy = "items")
+    var categorise: MutableList<Category> = mutableListOf(),
 
-  var name: String? = null
-  var price: Int? = null
-  var stockQuantity: Int? = null
+    @Id @GeneratedValue
+    @Column(name = "item_id")
+    private var id: Long? = null
+) {
+    // 비지니스 로직
+    fun addStock(quantity: Int) {
+        this.stockQuantity -= quantity
+    }
 
-  @ManyToMany(mappedBy = "items")
-  var categorise: MutableList<Category>? = null
+    fun removeStock(quantity: Int) {
+        val restStock = this.stockQuantity - quantity
+        if (restStock < 0) {
+            throw NotEnoughStockException("need more stock")
+        }
+        this.stockQuantity -= quantity
+    }
 }
