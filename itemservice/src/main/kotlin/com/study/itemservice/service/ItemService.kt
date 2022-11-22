9a7681dd2286173ff0d93d64e1358465edb54bcc
rnfkaus8 <kth4350@gmail.com>
2022-11-22 19:46:15 +0900

    내부에서만 사용하는 repository 필드 private 추가

--- itemservice/src/main/kotlin/com/study/itemservice/service/ItemService.kt
+++ itemservice/src/main/kotlin/com/study/itemservice/service/ItemService.kt
@@ -8,7 +8,7 @@ import org.springframework.transaction.annotation.Transactional
 @Service
 @Transactional(readOnly = true)
 class ItemService(
-    val itemRepository: ItemRepository
+    private val itemRepository: ItemRepository
 ) {
 
     @Transactional
