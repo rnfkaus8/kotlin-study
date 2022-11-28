51797dceb3455a0a05f4e55205ced218151165da
rnfkaus8 <kth4350@gmail.com>
2022-11-28 21:15:00 +0900

    주문 검색 기능 추가

--- itemservice/src/main/kotlin/com/study/itemservice/repository/OrderRepository.kt
+++ itemservice/src/main/kotlin/com/study/itemservice/repository/OrderRepository.kt
@@ -1,8 +1,14 @@
 package com.study.itemservice.repository
 
+import com.study.itemservice.domain.Member
 import com.study.itemservice.domain.Order
+import com.study.itemservice.domain.OrderSearch
 import org.springframework.stereotype.Repository
+import org.springframework.util.StringUtils
 import javax.persistence.EntityManager
+import javax.persistence.TypedQuery
+import javax.persistence.criteria.*
+
 
 @Repository
 class OrderRepository(
@@ -17,7 +23,43 @@ class OrderRepository(
         return em.find(Order::class.java, id);
     }
 
-//     fun findAll(): MutableList<Order> {
-//
-//     }
+     fun findAllByString(orderSearch: OrderSearch): MutableList<Order> {
+         var jpql: String = "select o From Order o join o.member m"
+         var isFirstCondition: Boolean = true
+
+         if (orderSearch.orderStatus != null) {
+             if (isFirstCondition) {
+                 jpql += " where"
+                 isFirstCondition = false
+             } else {
+                 jpql += " and"
+             }
+             jpql += " o.status = :status"
+         }
+
+         if (StringUtils.hasText(orderSearch.memberName)) {
+             if (isFirstCondition) {
+                 jpql += " where"
+                 isFirstCondition = false
+             } else {
+                 jpql += " and"
+             }
+             jpql += " m.name like :name"
+         }
+
+         var query: TypedQuery<Order> = em.createQuery(jpql, Order::class.java)
+             .setMaxResults(1000)
+
+         if (orderSearch.orderStatus != null) {
+             query = query.setParameter("status", orderSearch.orderStatus)
+         }
+
+         if (StringUtils.hasText(orderSearch.memberName)) {
+             query = query.setParameter("name", orderSearch.memberName)
+         }
+
+         return query.resultList
+
+     }
+
 }
