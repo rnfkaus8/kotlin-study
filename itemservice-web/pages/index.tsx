5614a290ad8323155b25b1db269346e19804d361
rnfkaus8 <kth4350@gmail.com>
2022-12-01 23:22:32 +0900

    init next project

--- itemservice-web/pages/index.tsx
+++ itemservice-web/pages/index.tsx
@@ -1,10 +1,5 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import React from 'react';
-import styles from '../styles/Home.module.css';
-
-function HomePage() {
-  return <div>Hello World</div>;
+const home = () => {
+  return "hi"
 }
 
-export default HomePage;
+export default home()
