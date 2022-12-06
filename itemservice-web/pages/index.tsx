3d43d791fb5f584953374f225ccc690a7b252594
rnfkaus8 <kth4350@gmail.com>
2022-12-06 21:36:29 +0900

    add to home page

--- itemservice-web/pages/index.tsx
+++ itemservice-web/pages/index.tsx
@@ -1,10 +1,25 @@
 import React, { useState } from 'react';
-import NavBar from '../components/NavBar';
+import Link from 'next/link';
 
 const Home: React.FC = () => {
   return (
     <div>
-      <h1>hello </h1>
+      <h1>Hello Shop</h1>
+      <p>회원 기능</p>
+      <p>
+        <Link href="/members/new">회원 가입</Link>
+        <Link href="/members">회원 목록</Link>
+      </p>
+      <p>상품 기능</p>
+      <p>
+        <Link href="/items/new">상품 등록</Link>
+        <Link href="/items">상품 목록</Link>
+      </p>
+      <p>주문 기능</p>
+      <p>
+        <Link href="/order">상품 주문</Link>
+        <Link href="/orders">주문 내역</Link>
+      </p>
     </div>
   );
 };
