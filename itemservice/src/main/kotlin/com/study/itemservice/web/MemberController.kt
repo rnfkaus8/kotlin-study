59cf4f35bd0867a510efbf7279b49cf6099c96e5
rnfkaus8 <kth4350@gmail.com>
2022-12-12 21:16:58 +0900

    add to create Member controller

--- itemservice/src/main/kotlin/com/study/itemservice/web/MemberController.kt
+++ itemservice/src/main/kotlin/com/study/itemservice/web/MemberController.kt
@@ -1,19 +1,33 @@
 package com.study.itemservice.web
 
+import com.study.itemservice.domain.Address
+import com.study.itemservice.domain.Member
+import com.study.itemservice.service.MemberService
 import lombok.extern.slf4j.Slf4j
 import org.slf4j.LoggerFactory
-import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.validation.BindingResult
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RestController
+import javax.validation.Valid
 
-@Controller
+@RestController
 @Slf4j
-class MemberController {
+class MemberController(
+    private val memberService: MemberService
+) {
 
     private val log = LoggerFactory.getLogger(javaClass)
 
-    @RequestMapping("/")
-    fun home(): String {
-        log.info("home controller")
-        return "home"
+    @PostMapping(value = ["/members/new"])
+    fun create(@Valid form: MemberForm): String {
+        log.info("name : ${form.name}")
+        log.info("city : ${form.city}")
+        log.info("street : ${form.street}")
+        log.info("zipcode : ${form.zipcode}")
+        val address: Address = Address(city = form.city, street = form.street, zipcode = form.zipcode)
+        val member: Member = Member(name = form.name, address = address)
+        memberService.join(member)
+        return "hello"
     }
+
 }
