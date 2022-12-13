387a1e26ce07a1e60087716165ecc0312d48c8e8
rnfkaus8 <kth4350@gmail.com>
2022-12-13 23:02:38 +0900

    fetch to create member api

--- itemservice-web/pages/members/new.tsx
+++ itemservice-web/pages/members/new.tsx
@@ -1,46 +1,62 @@
 import React from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { Simulate } from 'react-dom/test-utils';
+import error = Simulate.error;
 
+type FormData = {
+  name: string;
+  city: string;
+  street: string;
+  zipcode: string;
+};
 const MemberCreateForm = () => {
+  const { handleSubmit, register } = useForm<FormData>();
+
+  const onSubmit: SubmitHandler<FormData> = (data) => {
+    console.log(JSON.stringify(data));
+    fetch('/api/members/new', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json;charset=UTF-8',
+      },
+      body: JSON.stringify(data),
+    })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
-    <form action="http://localhost:8080/members/new" method="post">
+    <form onSubmit={handleSubmit(onSubmit)}>
       <label htmlFor="name">
         이름
         <input
-          type="text"
-          id="name"
-          name="name"
           placeholder="이름을 입력하세요"
-          required
+          {...register('name', { required: true })}
         />
       </label>
       <label htmlFor="city">
         도시
         <input
-          type="text"
-          id="city"
-          name="city"
+          {...register('city', { required: true })}
           placeholder="도시를 입력하세요"
-          required
         />
       </label>
       <label htmlFor="street">
         거리
         <input
-          type="text"
-          id="street"
-          name="street"
+          {...register('street', { required: true })}
           placeholder="거리를 입력하세요"
-          required
         />
       </label>
       <label htmlFor="zipcode">
         우편번호
         <input
-          type="text"
-          id="zipcode"
-          name="zipcode"
+          {...register('zipcode', { required: true })}
           placeholder="우편번호를 입력하세요"
-          required
         />
       </label>
       <button type="submit">Submit</button>
