1f52a8ba19fc24d4ff61f749e220199726ee2575
kimtaehyun-minding <kth4350@minding.today>
2023-02-07 19:48:15 +0900

    스프링 부트 jpa with kotlin start

---
+++ kotlin/src/test/kotlin/study/jpa/kotlin/KotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package study.jpa.kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
