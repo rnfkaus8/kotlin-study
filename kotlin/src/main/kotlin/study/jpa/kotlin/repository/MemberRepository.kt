623bd604def1ffd5ed2de4f36a21713b478fe9c4
rnfkaus8 <kth4350@gmail.com>
2023-02-12 18:47:17 +0900

    회원 레포지토리 추가

---
+++ kotlin/src/main/kotlin/study/jpa/kotlin/repository/MemberRepository.kt
@@ -0,0 +1,31 @@
+package study.jpa.kotlin.repository
+
+import jakarta.persistence.EntityManager
+import jakarta.persistence.PersistenceContext
+import org.springframework.stereotype.Repository
+import study.jpa.kotlin.domain.Member
+
+@Repository
+class MemberRepository(
+  @PersistenceContext
+  val em: EntityManager
+) {
+
+  fun save(member: Member) {
+    em.persist(member)
+  }
+
+  fun findOne(id: Long): Member {
+    return em.find(Member::class.java, id);
+  }
+
+  fun findAll(): List<Member> {
+    return em.createQuery("select m from Member m", Member::class.java).resultList
+  }
+
+  fun findByName(name: String): List<Member> {
+    return em.createQuery("select m from Member m where m.name = :name", Member::class.java)
+      .setParameter("name", name)
+      .resultList
+  }
+}
